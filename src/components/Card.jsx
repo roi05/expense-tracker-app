368ec8c45ad7d0b1fa2008eb351e990dd356c97a
Roi Buenaventura <roibuenaventura05@gmail.com>
2023-04-20 11:37:22 +0800

    edit css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,9 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import numeral from 'numeral';
 import { useExpenseData } from '../hooks/useExpenseData';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
 
 const Card = () => {
   const { data } = useExpenseData();
-
-  const theme = useTheme();
-  const isXs = useMediaQuery(theme.breakpoints.down('sm'));
 
   const expenseTotal = data?.data.expense
     .filter(item => item.type === 'expense')
@@ -33,40 +28,40 @@ const Card = () => {
             My Balance
           </Typography>
           <Typography
-            variant={isXs ? 'h5' : 'h3'}
-            sx={{ fontWeight: 500 }}
+            variant='h5'
+            sx={{ fontWeight: 500, mb: 3 }}
             gutterBottom>
             {`₱${numeral(allTotal).format('0,0.00')}`}
           </Typography>
 
           <Box>
-            <Grid
-              container
-              spacing={2}>
+            <Grid container>
               <Grid
                 item
-                xs={6}>
+                xs={12}
+                sm={6}>
                 <Typography
                   variant='overline'
                   component='h5'>
                   Income
                 </Typography>
                 <Typography
-                  variant={isXs ? 'h5' : 'h3'}
+                  variant='h5'
                   gutterBottom>
                   {`+ ₱${numeral(savingTotal).format('0,0.00')}`}
                 </Typography>
               </Grid>
               <Grid
                 item
-                xs={6}>
+                xs={12}
+                sm={6}>
                 <Typography
                   variant='overline'
                   component='h5'>
                   Expense
                 </Typography>
                 <Typography
-                  variant={isXs ? 'h5' : 'h3'}
+                  variant='h5'
                   gutterBottom>
                   {`- ₱${numeral(expenseTotal).format('0,0.00')}`}
                 </Typography>
